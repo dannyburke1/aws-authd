f03be6f21d5ce9d662ecef47ee6b8f1eef3c99ef
Danny Burke <dburke9607@gmail.com>
2021-03-14 00:26:31 +0000

    Adding extra configuration for API Gateway, currently not working

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,40 @@
 import * as cdk from '@aws-cdk/core';
-import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as api from '@aws-cdk/aws-apigateway';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const accountAPI = new apigateway.RestApi(this, 'aws-account-api');
+    const accountAPI = new api.RestApi(this, 'aws-account-api');
 
     accountAPI.root.addMethod('ANY');
+    const integration = new accountAPI.HttpIntegration('http://amazon.com');
+
+    const v1 = accountAPI.root.addResource('v1');
+    const echo = v1.addResource('echo');
+    const echoMethod = echo.addMethod('GET', integration, { apiKeyRequired: true });
+    const key = accountAPI.addApiKey('ApiKey');
+
+    const plan = accountAPI.addUsagePlan('UsagePlan', {
+      name: 'Easy',
+      apiKey: key,
+      throttle: {
+        rateLimit: 10,
+        burstLimit: 2
+      }
+    });
+
+  plan.addApiStage({
+    stage: accountAPI.deploymentStage,
+    throttle: [
+      {
+        method: echoMethod,
+        throttle: {
+          rateLimit: 10,
+          burstLimit: 2
+        }
+      }
+    ]
+   });
   }
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,4 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.2.1 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.1.2 // indirect
 )
+
