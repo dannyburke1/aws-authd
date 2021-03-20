c37efc1fc990504d7bc498b80f97404e1d2d46bf
Danny Burke <dburke9607@gmail.com>
2021-03-20 22:32:18 +0000

    Add DynamoDB table and DataSource for GraphQL

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as appsync from '@aws-cdk/aws-appsync';
+import * as ddb from '@aws-cdk/aws-dynamodb';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -14,9 +15,20 @@ export class CdkStack extends cdk.Stack {
     new cdk.CfnOutput(this, "GraphQLAPIURL", {
      value: api.graphqlUrl
     });
-    
+
     new cdk.CfnOutput(this, "Stack Region", {
       value: this.region
     });
+
+    const accountTable = new ddb.Table(this, 'AwsAccountsTable', {
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: 'id',
+        type: ddb.AttributeType.STRING,
+      },
+    });
+
+    const dynamoData = api.addDynamoDbDataSource('dynamoDBDataSource', accountTable);
    }
+
 }
